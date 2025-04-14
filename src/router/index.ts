1ad550a73733a8ddd40712cc1ebe0149434c5595
Inés Betrián <betrianines18@gmail.com>
2025-04-14 19:57:44 +0200

    BreadCrumb

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,15 @@ const router = createRouter({
           path: 'projects',
           name: 'projects',
           component: () => import('@/modulorum/projects/views/ProjectsView.vue')
+        },
+
+        {
+          path: 'projects/:id',
+          props: true,
+          name: 'project',
+          component: () => import('@/modulorum/projects/views/ProjectView.vue')
         }
+
       ]
     }
   ],
